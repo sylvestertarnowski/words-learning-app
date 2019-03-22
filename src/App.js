6c3031c82e27aa682c6c81e3e92860ae656d7711
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2019-03-22 07:30:35 +0100

    Proxy for node server in package.json

--- src/App.js
+++ src/App.js
@@ -3,10 +3,34 @@ import Words from "./Words";
 import "./index.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: null
+    }
+  }
+
+  componentDidMount() {
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
+  
   render() {
     return (
       <div>
         <Words />
+        <p>{this.state.data}</p>
       </div>
     )
   }
