fba3de6907ea944bed737238c247e3e1cc47cdb7
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2019-03-27 07:54:47 +0100

    Add POST method to save current list in database

--- src/PostWords.js
+++ src/PostWords.js
@@ -4,18 +4,55 @@ class PostWords extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            name: "",
+            language: "",
             list: props.data
         }
         this.handleSave = this.handleSave.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    handleSave() {
-        console.log(this.state.list)
+    handleSave(event) {
+        event.preventDefault();
+        fetch('/words/add' , {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.state)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err))
+    }
+
+    handleChange(event) {
+        const {name, value} = event.target;
+        this.setState({
+            [name]: value
+        })
     }
 
     render() { 
         return (
-            <button type="submit" onClick={this.handleSave}>Save Words</button>
+            <div>
+                <input 
+                    type="text" 
+                    value={this.state.name} 
+                    name="name" 
+                    onChange={this.handleChange}
+                    placeholder="Unique List Name" 
+                />
+                <input
+                    type="text"
+                    value={this.state.language}
+                    name="language"
+                    onChange={this.handleChange} 
+                    placeholder="Language"
+                />
+                <button type="submit" onClick={this.handleSave}>Save Words</button>
+            </div>
         )   
     }
 }
